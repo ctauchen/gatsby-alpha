abcde1cdf8b301a646abe1260d772a504d6ea5f4
Christopher Tauchen <chris.tauchen@tigera.io>
2023-02-12 22:03:19 +0000

    tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,9 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-alpha`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making PICKLES!!! by following the Gatsby Tutorial.</p>
             <p> This build witll fail.</p>
+            <StaticImage
+                alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+                src="../images/image.webp"
+            />
         </Layout>
     )
 }
